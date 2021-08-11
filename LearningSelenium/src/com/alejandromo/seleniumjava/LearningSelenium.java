218d4ddf8b02d1afb2de215cf5a9da5711f74231
Alex Márquez <alejandro.marquez@thomsonreuters.com>
2021-08-11 17:39:14 -0500

    Updates on website URL. Practice Selenium

--- LearningSelenium/src/com/alejandromo/seleniumjava/LearningSelenium.java
+++ LearningSelenium/src/com/alejandromo/seleniumjava/LearningSelenium.java
@@ -1,14 +1,53 @@
 package com.alejandromo.seleniumjava;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
 
 public class LearningSelenium {
     public static void main(String[] args) {
-        WebDriver driver = new ChromeDriver();
-        String url = "http://demo.automationtesting.in/Register.html";
 
+        //Creating a new web driver for Chrome Browser
+        WebDriver driver = new ChromeDriver();
+        /*
+         * We can define a headless property with
+         * chromeOptions.addArguments("--headless")
+         */
+        //Defined expected results for Demo site
+        String url = "https://demo.opencart.com/";
+        String expectedTitle = "Your Store";
+
+        //Maximize browser screen
+        driver.manage().window().maximize();
+        //Open URL on browser
         driver.get(url);
+
+        //getTitle() returns website title on tags <title>
+        if (expectedTitle.contentEquals(driver.getTitle())){
+            System.out.println(driver.getTitle());
+            System.out.println("Correct site 👍");
+
+            //Using findElement() by id
+            WebElement webElement = driver.findElement(By.id("logo"));
+            String logoTitle = webElement.getText();
+            System.out.println("Title with id = 'logo': " + logoTitle);
+
+            //Using findElements() by classname
+            List<WebElement> webElementList = driver.findElements(By.className("dropdown"));
+            System.out.println("\nElements with classname = 'dropdown': ");
+            for (WebElement element :
+                    webElementList) {
+                System.out.println(element.getText());
+            }
+        } else {
+            System.out.println("Wrong site 👎");
+        }
+        //Close browser
         driver.close();
+        //Ending test
+        driver.quit();
     }
 }
