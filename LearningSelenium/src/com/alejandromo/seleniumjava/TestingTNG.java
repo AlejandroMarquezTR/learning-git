b27f33d5b98acc5a1513ab1363890734ec9f73ec
Alex Márquez <alejandro.marquez@thomsonreuters.com>
2021-08-20 14:46:46 -0500

    Adding TesNG class

---
+++ LearningSelenium/src/com/alejandromo/seleniumjava/TestingTNG.java
@@ -0,0 +1,10 @@
+package com.alejandromo.seleniumjava;
+
+import org.testng.annotations.Test;
+
+public class TestingTNG {
+    @Test(description = "Get title for webpage")
+    void getTitle(){
+
+    }
+}
